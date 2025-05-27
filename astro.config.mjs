5b2f7cc986b67899419e3903c48d2915d6e90876
hisenberg <me@someine.com>
2025-05-27 14:04:31 -0700

    name typo fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://rustohidev.github.io',
+  site: 'https://rustoshi.github.io',
   base: '/',
 });

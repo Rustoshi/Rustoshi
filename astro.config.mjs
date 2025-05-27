ebb890b7e518fb6b0575b16ea80a8f56305a5e83
Rustoshi <rustohidev@gmail.com>
2025-05-27 13:43:18 -0700

    Config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   site: 'https://rustohidev.github.io',
-  base: '/rustohidev.github.io',
+  base: '/',
 });
